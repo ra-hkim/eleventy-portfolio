a2fa614f7e808c257ecdf5bd5ff4f9d1d2416c44
rahkim <rahkimali2009@icloud.com>
2026-09-05 21:38:20 +0100

    fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
       pathPrefix: "/eleventy-portfolio/",
