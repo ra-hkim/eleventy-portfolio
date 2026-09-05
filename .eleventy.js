37bab572a4f72a27a6610423ac7430e4ef45a45e
rahkim <rahkimali2009@icloud.com>
2026-09-05 21:33:11 +0100

    fix 4

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
 
   return {
-      pathPrefix: "/eleventy-portolio/",
+      pathPrefix: "/eleventy-portfolio/",
         dir: {
             input: "src",
             includes: "_includes",
